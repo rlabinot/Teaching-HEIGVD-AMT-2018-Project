02214eee362dc1ae27f8818122779e308745491b
Romain Gallay <romain.gallay@heig-vd.ch>
2018-10-19 12:32:38 +0200

    update class Log + modif gitignore

--- webui/src/main/java/ch/heig/amt/gamification/model/Log.java
+++ webui/src/main/java/ch/heig/amt/gamification/model/Log.java
@@ -6,19 +6,19 @@ import java.util.LinkedList;
 public class Log {
 
     private int id;
+    private String user;
     private Timestamp date;
-    private String status;
-    private String action;
+    private String status; // e.g: INFO, SEVERE...
+    private String action; // e.g: login, add an application, reset a password...
     private String description;
-    private LinkedList<Log> logs;
 
-    public Log(int id, Timestamp date, String status, String action, String description) {
+    public Log(int id, String user, Timestamp date, String status, String action, String description) {
         this.id = id;
+        this.user = user;
         this.date = date;
         this.status = status;
         this.action = action;
         this.description = description;
-        this.logs = new LinkedList<>();
     }
 
     public int getId() {
@@ -61,12 +61,11 @@ public class Log {
         this.description = description;
     }
 
-    public LinkedList<Log> getLogs() {
-        return logs;
+    public String getUser() {
+        return user;
     }
 
-    public void setLogs(LinkedList<Log> logs) {
-        this.logs = logs;
+    public void setUser(String user) {
+        this.user = user;
     }
-
 }
