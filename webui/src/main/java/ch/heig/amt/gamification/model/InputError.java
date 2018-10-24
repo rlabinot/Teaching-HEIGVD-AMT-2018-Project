5bb8db8b7342d19e31fbe2c611eda6b91e54a85c
Ludwig <loic.frueh@gmail.com>
2018-10-24 12:40:42 +0200

    Change for the jsp template and first try for the login and index JSP

--- webui/src/main/java/ch/heig/amt/gamification/model/InputError.java
+++ webui/src/main/java/ch/heig/amt/gamification/model/InputError.java
@@ -2,9 +2,9 @@ package ch.heig.amt.gamification.model;
 
 public class InputError {
     private static final String EMPTY_FIELD_MESSAGE = "Field cannot be empty";
-    private static final String WRONG_FORMAT_EMAIL = "Invalid format for email";
-    private static final String WEAK_PASSWORD = "Password doesn't contain a number, an uppercase and 8 characters without blanks";
-    private static final String WRONG_LOGIN = "Wrong login or password";
+    private static final String WRONG_FORMAT_EMAIL_MESSAGE = "Invalid format for email";
+    private static final String WEAK_PASSWORD_MESSAGE = "Password doesn't contain a number, an uppercase and 8 characters without blanks";
+    private static final String WRONG_LOGIN_MESSAGE = "Wrong login or password";
     private static final String USER_ALREADY_REGISTERED = "User already registered";
     private static final String APP_ALREADY_REGISTERED = "App already registered";
 
@@ -107,15 +107,15 @@ public class InputError {
         return EMPTY_FIELD_MESSAGE;
     }
 
-    public static String getWrongFormatEmail() {
+    public static String getWrongFormatEmailMessage() {
         return WRONG_FORMAT_EMAIL;
     }
 
-    public static String getWeakPassword() {
+    public static String getWeakPasswordMessage() {
         return WEAK_PASSWORD;
     }
 
-    public static String getWrongLogin() {
+    public static String getWrongLoginMessage() {
         return WRONG_LOGIN;
     }
 
