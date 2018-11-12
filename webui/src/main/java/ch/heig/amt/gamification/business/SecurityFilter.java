76ec893f98b8a07f2d35c8c199dc39fbe519d2a7
Romain Gallay <romain.gallay@heig-vd.ch>
2018-11-12 17:41:36 +0100

    secure stuff, catch potential errors...

--- webui/src/main/java/ch/heig/amt/gamification/business/SecurityFilter.java
+++ webui/src/main/java/ch/heig/amt/gamification/business/SecurityFilter.java
@@ -4,6 +4,9 @@ import ch.heig.amt.gamification.model.InputError;
 import ch.heig.amt.gamification.model.User;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.mail.MessagingException;
 import javax.servlet.Filter;
@@ -14,6 +17,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SecurityFilter implements Filter {
     /**
@@ -24,11 +28,17 @@ public class SecurityFilter implements Filter {
      * @exception IOException if an input/output error occurs
      * @exception ServletException if a servlet error occurs
      */
+
+    // list of actions associated with userServlet that a non-admin can execute
+    private List<String> non_admin_actions = Arrays.asList("changePassword", null);
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession httpSession = httpRequest.getSession();
         String path = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
+        String action = request.getParameter("action");
+        Boolean isAdmin = Boolean.TRUE == httpSession.getAttribute("isAdmin");
 
         // we use a whitelist system to define which url are protected or not
         boolean isTargetUrlProtected = true;
@@ -38,7 +48,8 @@ public class SecurityFilter implements Filter {
             isTargetUrlProtected = false;
         } else if (path.startsWith("/login")) {
             isTargetUrlProtected = false;
-        } else if (path.startsWith("/user") && !path.contains("action")) {
+        } else if (path.startsWith("/user") && action == null) {
+            // register user
             isTargetUrlProtected = false;
         }
 
@@ -46,7 +57,7 @@ public class SecurityFilter implements Filter {
          * If the user has been authenticated before, then the AuthenticationServlet has placed
          * an object (in this case a String) in the HTTP session. We can retrieve it.
          */
-        String email = (String) httpRequest.getSession().getAttribute("email");
+        String email = (String) httpSession.getAttribute("email");
 
         if (email == null && isTargetUrlProtected) {
             /*
@@ -57,8 +68,8 @@ public class SecurityFilter implements Filter {
 
         } else {
 
-            boolean mustChangePassword = Boolean.TRUE == httpRequest.getSession().getAttribute("mustChangePassword");
-            boolean isActive= Boolean.TRUE == httpRequest.getSession().getAttribute("isActive");
+            boolean mustChangePassword = Boolean.TRUE == httpSession.getAttribute("mustChangePassword");
+            boolean isActive= Boolean.TRUE == httpSession.getAttribute("isActive");
             InputError error = new InputError();
 
             if (!isActive && email != null) {
@@ -68,12 +79,19 @@ public class SecurityFilter implements Filter {
 
             } else if (mustChangePassword && email != null) {
                 // the filter should not redirect the action to change pwd
-                if(httpRequest.getMethod().equals("POST")) {
+                if(path.startsWith("/user") && action.equals("changePassword")) {
                     chain.doFilter(request, response);
                 } else {
                     request.getRequestDispatcher("/WEB-INF/pages/chngPassword.jsp").forward(request, response);
                 }
             } else {
+                // check if admin privilege is needed
+                if(path.startsWith("/user") && !non_admin_actions.contains(action)){
+                    if(!isAdmin){
+                        request.setAttribute("pageTitle", "404 Page");
+                        request.getRequestDispatcher("/WEB-INF/pages/404.jsp").forward(request, response);
+                    }
+                }
 
                 /*
                  * We authorize the access, so we can tell the request processing pipeline to
