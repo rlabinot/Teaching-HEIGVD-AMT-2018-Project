fa5166ea74eb1c25de2adb3544455cd0bbb20860
Dejvid Muaremi <dejvid95@hotmail.com>
2018-11-13 23:58:58 +0100

    rm -rf /*

--- webui/src/main/java/ch/heig/amt/gamification/business/EmailSenderLocal.java
+++
@@ -1,9 +0,0 @@
-package ch.heig.amt.gamification.business;
-
-import javax.ejb.Local;
-import javax.mail.MessagingException;
-
-@Local
-public interface EmailSenderLocal {
-    public void send(String to, String subject, String body) throws MessagingException;
-}
