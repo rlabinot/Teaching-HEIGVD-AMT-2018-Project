36e0ddb58f387e2762218b7649fc940133335862
Labinot Rashiti <labinot.rashiti@heig-vd.ch>
2018-12-14 09:32:34 +0100

    adding some repositories

---
+++ swagger/spring-server/src/main/java/ch/heig/amt/gamification/repositories/BadgeRepository.java
@@ -0,0 +1,11 @@
+package ch.heig.amt.gamification.repositories;
+
+import ch.heig.amt.gamification.entities.FruitEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Labinot Rashiti on 14/12/18.
+ */
+public interface BadgeRepository extends CrudRepository<FruitEntity, Long>{
+
+}
