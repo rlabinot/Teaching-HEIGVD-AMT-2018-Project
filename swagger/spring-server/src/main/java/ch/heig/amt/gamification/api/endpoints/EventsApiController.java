ead03f6138fcd9be1f4cda36eebf670de14de811
Ludwig <loic.frueh@gmail.com>
2019-01-24 19:52:47 +0100

    Avancement event

--- swagger/spring-server/src/main/java/ch/heig/amt/gamification/api/endpoints/EventsApiController.java
+++ swagger/spring-server/src/main/java/ch/heig/amt/gamification/api/endpoints/EventsApiController.java
@@ -2,16 +2,14 @@ package ch.heig.amt.gamification.api.endpoints;
 
 import ch.heig.amt.gamification.api.EventsApi;
 import ch.heig.amt.gamification.api.model.Event;
-import ch.heig.amt.gamification.entities.ApplicationEntity;
-import ch.heig.amt.gamification.entities.EventEntity;
-import ch.heig.amt.gamification.entities.RuleEntity;
-import ch.heig.amt.gamification.entities.UserEntity;
-import ch.heig.amt.gamification.repositories.ApplicationRepository;
-import ch.heig.amt.gamification.repositories.EventRepository;
-import ch.heig.amt.gamification.repositories.RuleRepository;
+import ch.heig.amt.gamification.entities.*;
+import ch.heig.amt.gamification.repositories.*;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -32,26 +30,64 @@ public class EventsApiController implements EventsApi {
     @Autowired
     ApplicationRepository applicationRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    BadgeRewardRepository badgeRewardRepository;
+
     @Override
-    public ResponseEntity<Object> createEvent(Event event) {
+    public ResponseEntity<Object> sendEvent(@ApiParam(value = "" ,required=true ) @RequestBody Event event,
+                                     @ApiParam(value = "" ,required=true ) @RequestHeader(value="apiKey", required=true) String apiKey) {
         // Transform the event to an entity and save it for future state rules
         EventEntity newEventEntity = toEventEntity(event);
 
         // Get the application that send this event
-        ApplicationEntity application = applicationRepository.findByApplicationName(); // TODO
+        ApplicationEntity application = applicationRepository.findByApplicationName(apiKey);
         if (application == null) {
             return null;
         }
 
-        // Get the user which sends the event
-        UserEntity user = newEventEntity.get
+        // Get the user which sends the event (if he doesn't exist => create it)
+        UserEntity user = newEventEntity.getUser();
+        if (userRepository.findByUserIdAndAndApplication_ApplicationName(user.getUserId(), apiKey) == null) {
+            user.setApplication(application);
+            System.out.println("User " + user.getUserId() + "created");
+        }
 
-
-        // Get the rules that match this event type
-        List<RuleEntity> rules = ruleRepository.findAll(); // TODO
+        // Get all the rules from the app and which are triggered by this event type
+        List<RuleEntity> rules = ruleRepository.findAllByEventTriggerAndApplication_ApplicationName(event.getEventType(), apiKey);
         if (rules.isEmpty()) {
             return null;
         }
+
+
+        for (RuleEntity rule : rules) {
+            // Get the badges from the rules
+            BadgeEntity badge = rule.getBadge();
+            if (badge != null) {
+                BadgeRewardEntity badgeReward = new BadgeRewardEntity();
+                badgeReward.setUser(user);
+                badgeReward.setBadge(badge);
+                badgeRewardRepository.save(badgeReward);
+                System.out.println("User + " + user.getUserId() + " win " + badge.getBadgeName() + "badge.");
+            }
+
+            // Get the pointscales from the rules
+            PointScaleEntity pointScale = rule.getPointScale();
+            if (pointScale != null) {
+                PointScaleRewardEntity pointScaleReward = new PointScaleRewardEntity();
+                pointScaleReward.setUser(user);
+                pointScaleReward.setPointScale(pointScale);
+                pointScaleReward
+            }
+        }
+
+        // Get the pointscales from the rules
+
+
+
+
 
 
 
