c88f00cd2d0d489a1f5cc086d0981caff3c2611b
Dejvid Muaremi <dejvid95@hotmail.com>
2019-01-24 21:28:24 +0100

    Specific find for the badges

--- swagger/spring-server/src/main/java/ch/heig/amt/gamification/repositories/BadgeRepository.java
+++ swagger/spring-server/src/main/java/ch/heig/amt/gamification/repositories/BadgeRepository.java
@@ -1,11 +1,15 @@
 package ch.heig.amt.gamification.repositories;
 
+import ch.heig.amt.gamification.api.model.Badge;
 import ch.heig.amt.gamification.entities.BadgeEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 /**
  * Created by Labinot Rashiti on 14/12/18.
  */
 public interface BadgeRepository extends PagingAndSortingRepository<BadgeEntity, Integer>{
-
+    List<BadgeEntity> findAllByApplicationApplicationName(String apiKey);
+    Badge findByBadgeIdAndApplicationApplicationName(int id, String apiKey);
 }
