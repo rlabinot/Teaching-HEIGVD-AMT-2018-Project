f24dd49d7151ef75a9f9991bb1fe1031ea9c9711
Dejvid Muaremi <dejvid95@hotmail.com>
2019-01-24 20:10:51 +0100

    lololololololo

--- swagger/spring-server/src/main/java/ch/heig/amt/gamification/repositories/PointScaleRewardRepository.java
+++ swagger/spring-server/src/main/java/ch/heig/amt/gamification/repositories/PointScaleRewardRepository.java
@@ -1,7 +1,9 @@
 package ch.heig.amt.gamification.repositories;
 
+import ch.heig.amt.gamification.entities.PointScaleEntity;
 import ch.heig.amt.gamification.entities.PointScaleRewardEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface PointScaleRewardRepository extends PagingAndSortingRepository<PointScaleRewardEntity, Integer> {
+    PointScaleRewardEntity findPointScaleRewardEntityByPointScale_PointScaleNameAndUser_UserId(String pointScaleName, int userId);
 }
