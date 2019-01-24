ed30911c592b85eedee308bc332f654a13696ad4
Dejvid Muaremi <dejvid95@hotmail.com>
2019-01-24 18:10:55 +0100

    go lololo

--- swagger/spring-server/src/main/java/ch/heig/amt/gamification/repositories/UserRepository.java
+++ swagger/spring-server/src/main/java/ch/heig/amt/gamification/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package ch.heig.amt.gamification.repositories;
 
+import ch.heig.amt.gamification.entities.UserEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends PagingAndSortingRepository<UserRepository, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Integer> {
+    UserEntity findByUserIdAndAndApplication_ApplicationName(int userId, String applicationName);
+    
 }
