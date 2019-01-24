2de2520bd62ed0ee24b3a8509360a903375f1d00
Dejvid Muaremi <dejvid95@hotmail.com>
2019-01-24 15:59:58 +0100

    Revert "go lolo"
    This reverts commit be2044a66c6e0f64dbd0f87ba77da6e8579879a7.

--- swagger/spring-server/src/main/java/ch/heig/amt/gamification/repositories/ApplicationRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.heig.amt.gamification.repositories;
-
-import ch.heig.amt.gamification.entities.ApplicationEntity;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ApplicationRepository extends PagingAndSortingRepository<ApplicationEntity, Integer> {
-    ApplicationEntity findByApplicationName(String name);
-}
--- swagger/spring-server/src/main/java/ch/heig/amt/gamification/repositories/PointScaleRepository.java
+++ swagger/spring-server/src/main/java/ch/heig/amt/gamification/repositories/PointScaleRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
  */
 public interface PointScaleRepository extends PagingAndSortingRepository<PointScaleEntity, Integer>{
 
+    List<PointScaleRepository> findAllByPointScaleNameContains(String name);
+
 }
